ed1f9ed602156da17fa08c0b0f619e9a0245af36
nincandy <ninjagh1258@gmail.com>
2026-08-01 02:57:25 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore, collection, addDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD2XppnhpwrtmBfcrz6uKtET1WsPi-F-XA",
+  authDomain: "text-img-43f01.firebaseapp.com",
+  projectId: "text-img-43f01",
+  storageBucket: "text-img-43f01.firebasestorage.app",
+  messagingSenderId: "926891045186",
+  appId: "1:926891045186:web:3b1417e88a9c26180cd8db",
+  measurementId: "G-6YBJQB4BEL"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+// Export ตัวแปรทั้งหมดที่ต้องใช้ออกไป
+export { db, storage, collection, addDoc, onSnapshot, ref, uploadBytes, getDownloadURL };
